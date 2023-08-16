992eecdbda82338d2395745312f3805019022839
julioale21 <30907777+julioale21@users.noreply.github.com>
2023-08-15 21:48:30 -0300

    lexiconia

---
+++ lexiconia.rb
@@ -0,0 +1,50 @@
+=begin
+Introduction
+You work for a company that makes an online multiplayer game called Lexiconia.
+
+To play the game, each player is given 13 letters, which they must rearrange to create words. Different letters have different point values, since it's easier to create words with some letters than others.
+
+The game was originally launched in English, but it is very popular, and now the company wants to expand to other languages as well.
+
+Different languages need to support different point values for letters. The point values are determined by how often letters are used, compared to other letters in that language.
+
+For example, the letter 'C' is quite common in English, and is only worth 3 points. But in Norwegian it's a very rare letter, and is worth 10 points.
+
+To make it easier to add new languages, your team needs to change the way letters and their point values are stored in the game.
+
+Instructions
+Your task is to change the data format of letters and their point values in the game.
+
+Currently, letters are stored in groups based on their score, in a one-to-many mapping.
+
+1 point: "A", "E", "I", "O", "U", "L", "N", "R", "S", "T",
+2 points: "D", "G",
+3 points: "B", "C", "M", "P",
+4 points: "F", "H", "V", "W", "Y",
+5 points: "K",
+8 points: "J", "X",
+10 points: "Q", "Z",
+This needs to be changed to store each individual letter with its score in a one-to-one mapping.
+
+"a" is worth 1 point.
+"b" is worth 3 points.
+"c" is worth 3 points.
+"d" is worth 2 points.
+etc.
+As part of this change, the team has also decided to change the letters to be lower-case rather than upper-case.
+
+=end
+
+module ETL
+    def self.transform(old)
+      letter_scores = {}
+  
+      old.each do |score, letters|
+        letters.each do |letter|
+          letter_scores[letter.downcase] = score
+        end
+      end
+  
+      return letter_scores
+    end
+end
